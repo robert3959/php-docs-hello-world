cfdb8c57e0e88b556c3ad04d7bc34af93ea0bcfa
robert3959 <163513722+robert3959@users.noreply.github.com>
2024-03-15 20:28:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 
 echo 'Twoje IP: '.$_SERVER['REMOTE_ADDR'];
 echo '  </p>  ';
-echo 'Drugie IP: '.$_SERVER['HTTP_X_FORWARDED_FOR'];
+$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+$ip_parts = explode(':', $ip);
+$cleaned_ip = trim($ip_parts[0]);
+
+echo 'Drugie IP: '.$cleaned_ip;
+
 echo '  </p>  ';
 echo 'Host: '.gethostbyaddr($_SERVER['REMOTE_ADDR']);
 echo '  ';
